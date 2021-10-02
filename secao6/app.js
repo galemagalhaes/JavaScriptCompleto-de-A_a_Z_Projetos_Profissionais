bf01323127f9d1fc3ab60f3d25c0deaad382bd1e
galemagalhaes <g.alemagalhaes@gmail.com>
2021-10-02 10:47:37 -0300

    aplica função ao sistema do banco

---
+++ secao6/app.js
@@ -0,0 +1,43 @@
+/*Melhorias feitas sobre o código inicial do banco, agora usando funções para 
+tornar mais fácil a reutiização do código */
+
+let nome;
+let sobrenome;
+let idade;
+let valorEmprestimo;
+let taxaDeJuros;
+let numAnos;
+let ehBomPagador;
+
+function cadastrarCliente(nomeCliente, sobrenomeCliente, idadeCliente, valorEmprestimoCliente, 
+    numAnosCliente, ehBomPagadorCliente) {
+      nome = nomeCliente;
+      sobrenome = sobrenomeCliente;
+      idade = idadeCliente;
+      valorEmprestimo = valorEmprestimoCliente;
+      numAnos = numAnosCliente;
+      ehBomPagador = ehBomPagadorCliente;
+      taxaDeJuros = defineTaxa(idadeCliente);
+}
+
+function defineTaxa(idadeCliente){
+    if(idade >= 18 && idade <= 25){
+      return 0.09;
+    } else if(idade >= 26 && idade <= 35){
+      return 0.08;
+    } else if(idade >=36 && idade <= 50){
+      return 0.07;
+    } else {
+      return 0.06;
+    } 
+}
+
+cadastrarCliente("Manuel", "Silva", 24, 150000, 2, true);
+console.log(nome);
+console.log(sobrenome);
+console.log(taxaDeJuros);
+
+cadastrarCliente("Edval", "Silveira", 56, 150000, 9, false);
+console.log(nome);
+console.log(sobrenome);
+console.log(taxaDeJuros);
