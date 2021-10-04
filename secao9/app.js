ec2ac9954d44a588f435d6349c2ad0122e19d491
galemagalhaes <g.alemagalhaes@gmail.com>
2021-10-04 17:30:57 -0300

    alternativa para uso de multiplos try/catch

--- secao9/app.js
+++ secao9/app.js
@@ -1,11 +1,3 @@
-try{
-  let x = y + 10;
-  console.log(x);
-}catch(erro){
-  console.log(erro);
-  console.log(erro.name);
-  console.log(erro.message);
-}
 
 try{
   let nome = "Manuel";
@@ -16,4 +8,17 @@ try{
   console.log(erro.message);
 }finally{
   console.log("Fim da instrução try/catch/finally")
+}
+
+
+//multiplos try/catch
+try{
+  let x = y + 10;
+  console.log(x);
+}catch(erro){
+  if(erro instanceof TypeError){
+    console.log("TypeError");
+  } else if(erro instanceof ReferenceError){
+    console.log("ReferenceError");
+  }
 }
