dc726aaf58610819392287bc27afaf803dea5e21
galemagalhaes <g.alemagalhaes@gmail.com>
2021-10-04 16:52:12 -0300

    try catch

---
+++ secao9/app.js
@@ -0,0 +1,19 @@
+try{
+  let x = y + 10;
+  console.log(x);
+}catch(erro){
+  console.log(erro);
+  console.log(erro.name);
+  console.log(erro.message);
+}
+
+try{
+  let nome = "Manuel";
+  console.log(nome.toUpperCase());
+}catch(erro){
+  console.log(erro);
+  console.log(erro.name);
+  console.log(erro.message);
+}finally{
+  console.log("Fim da instrução try/catch/finally")
+}
